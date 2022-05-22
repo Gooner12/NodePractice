a4ad5c3498fe2e17cdaa1c24071ded4d91a82336
Gooner12 <kcbiaksh@gmail.com>
2022-05-23 09:30:39 +1000

    Implemented the selected field functionality in the API

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -2,13 +2,13 @@ const Product = require("../models/product");
 const { search } = require("../routes/products");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).sort('-name price');
+  const products = await Product.find({}).select('name price');
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
   // pulling out only the properties you want to find
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
@@ -21,6 +21,7 @@ const getAllProducts = async (req, res) => {
   if(name) {
       queryObject.name= {$regex: name, $options: 'i'}; // options: i means case insensitive
   }
+  // sort
   // console.log(queryObject);
   let result = Product.find(queryObject); // we remove await from this statement as we need to chain sort with find
   if(sort) {
@@ -30,6 +31,13 @@ const getAllProducts = async (req, res) => {
   else {
     result = result.sort('createdAt');
   }
+
+  // selecting fields
+  if (fields) {
+    const fieldsList = fields.split(',').join(' ');
+    result = result.select(fieldsList);
+  }
+
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
