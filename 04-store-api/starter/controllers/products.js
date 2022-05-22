547971287c5554e16f8abecf7ed4900b6e1c769d
Gooner12 <kcbiaksh@gmail.com>
2022-05-23 09:47:49 +1000

    Implemented pagination

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -2,7 +2,11 @@ const Product = require("../models/product");
 const { search } = require("../routes/products");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).select('name price');
+  const products = await Product.find({})
+    .sort("name")
+    .select("name price")
+    // .limit(4)
+    // .skip(1);
   res.status(200).json({ products, nbHits: products.length });
 };
 
@@ -15,28 +19,34 @@ const getAllProducts = async (req, res) => {
   }
 
   if (company) {
-      queryObject.company = company;
+    queryObject.company = company;
   }
 
-  if(name) {
-      queryObject.name= {$regex: name, $options: 'i'}; // options: i means case insensitive
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" }; // options: i means case insensitive
   }
   // sort
   // console.log(queryObject);
   let result = Product.find(queryObject); // we remove await from this statement as we need to chain sort with find
-  if(sort) {
-      const sortList = sort.split(',').join(' ');
-      result = result.sort(sortList);
-  }
-  else {
-    result = result.sort('createdAt');
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
   }
 
   // selecting fields
   if (fields) {
-    const fieldsList = fields.split(',').join(' ');
+    const fieldsList = fields.split(",").join(" ");
     result = result.select(fieldsList);
   }
+
+  // implementing pagination. For this, we are not destructuring from the query object like before as we are taking the same name as in the query
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
 
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
