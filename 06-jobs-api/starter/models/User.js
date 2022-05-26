f6020ee7fe59b25f533574da6e050e9868070559
Gooner12 <kcbiaksh@gmail.com>
2022-05-26 11:17:19 +1000

    Used encrypted JWT_SECRET and expiresIn from .env file

--- 06-jobs-api/starter/models/User.js
+++ 06-jobs-api/starter/models/User.js
@@ -34,7 +34,7 @@ UserSchema.pre('save', async function(next) {
 
 // generate name using the instance method of schema. i.e. when an instance of user schema is created we can access methods function to create name
 UserSchema.methods.createJWT = function () {
-  return jwt.sign({userId:this._id, name: this.name}, 'jwtSecret', {expiresIn: '30d'});
+  return jwt.sign({userId:this._id, name: this.name}, process.env.JWT_SECRET, {expiresIn: process.env.JWT_LIFETIME});
 }
 
 module.exports = mongoose.model('User', UserSchema);
