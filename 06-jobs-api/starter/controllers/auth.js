b7844e575389db574a89a7f27e99266db5c0ae9a
Gooner12 <kcbiaksh@gmail.com>
2022-05-26 11:37:15 +1000

    Login authentication completed

--- 06-jobs-api/starter/controllers/auth.js
+++ 06-jobs-api/starter/controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, UnauthenticatedError } = require("../errors");
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body }); // here we want mongoose to do all the validation. this create method creates an instance of UserSchema
@@ -8,7 +9,23 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  res.send("login user");
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new BadRequestError("Please provide email and password");
+  }
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new UnauthenticatedError("Invalid Credentials");
+  }
+
+  // comparing password
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError("Invalid Credentials");
+  }
+
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
 };
 
 module.exports = {
