1a6969cde42b7174997e2c88188980cc8b6a17c6
Gooner12 <kcbiaksh@gmail.com>
2022-05-26 15:41:09 +1000

    Error handled using custom message for duplicate, validation and casting error

--- 06-jobs-api/starter/middleware/authentication.js
+++ 06-jobs-api/starter/middleware/authentication.js
@@ -6,11 +6,12 @@ const auth = async (req,res,next) => {
     // check header
     const authHeader = req.headers.authorization;
     if(!authHeader || !authHeader.startsWith('Bearer ')){
-        throw new UnauthenticatedError('Authentication Invalid');
+        throw new UnauthenticatedError('Authentication Invalidd');
     }
     const token = authHeader.split(' ')[1];
 
     try {
+        // console.log(authHeader);
         const payload = jwt.verify(token, process.env.JWT_SECRET);
 
          // alternative code to getting the user. Performs same as the code in line 21     
@@ -22,7 +23,7 @@ const auth = async (req,res,next) => {
         next();
     }
     catch (error) {
-        throw new UnauthenticatedError('Authentication Invalid');
+        throw new UnauthenticatedError('Authentication Invalids');
     }
 }
 
--- 06-jobs-api/starter/middleware/error-handler.js
+++ 06-jobs-api/starter/middleware/error-handler.js
@@ -1,10 +1,37 @@
 const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
+
+  let customError = {
+    // set default
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || 'Something went wrong try again later'
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message })
+  // }
+
+  // dealing with validation error message by iterating over an array of objects inside the errors object
+  if (err.name === 'ValidationError') {
+    // console.log(Object.values(err.errors));
+    customError.msg = Object.values(err.errors).map((item) => item.message).join(',');
+    customError.statusCode = 400;
+  }
+
+  // dealing with duplicate email values entered by user using a custom message
+  if (err.code && err.code === 11000) {
+    customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field, please choose another value`;
+    customError.statusCode = 400;
+  }
+
+  // dealing with casting errors using a custom message
+  if (err.name === "CastError") {
+    customError.msg = `No item found with id : ${err.value}`;
+    customError.statusCode = 404;
+  }
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+  return res.status(customError.statusCode).json({ msg: customError.msg });
 }
 
 module.exports = errorHandlerMiddleware
