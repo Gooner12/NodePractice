ae7438849057d64ee3a0ef7978bf721d46e8375e
Gooner12 <kcbiaksh@gmail.com>
2022-05-27 20:40:17 +1000

    Before the cloudinary setup

--- 07-file-upload/starter/app.js
+++ 07-file-upload/starter/app.js
@@ -6,6 +6,14 @@ const app = express();
 
 const fileUpload = require('express-fileupload');
 
+// use v2 for cloudinary
+const cloudinary = require('cloudinary').v2;
+cloudinary.config({
+  cloud_name:process.env.CLOUD_NAME,
+  api_key:process.env.CLOUD_API_KEY,
+  api_secret:process.env.CLOUD_API_SECRET,
+})
+
 // database
 const connectDB = require('./db/connect');
 
--- 07-file-upload/starter/controllers/uploadsController.js
+++ 07-file-upload/starter/controllers/uploadsController.js
@@ -3,8 +3,25 @@ const {StatusCodes} = require('http-status-codes');
 const CustomError = require('../errors');
 
 const uploadProductImage = async(req, res) => {
+    // check if file exists
+    // console.log(req.files);
+    if(!req.files) {
+        throw new CustomError.BadRequestError('No file Uploaded');
+    }
 
-    let productImage = req.files.image;
+    const productImage = req.files.image;
+
+    // checking if image has been uploaded    
+    if(!productImage.mimetype.startsWith('image')) {
+        throw new CustomError.BadRequestError('Please upload an image');
+    }
+
+    // checking the image size
+    const maxSize = 1024 * 1024;
+    if(productImage.size > maxSize) {
+        throw new CustomError.BadRequestError('Please upload image smaller than 1KB');
+    }
+
     const imagePath = path.join(__dirname,'../public/uploads/'+`${productImage.name}`);
 
     await productImage.mv(imagePath);
