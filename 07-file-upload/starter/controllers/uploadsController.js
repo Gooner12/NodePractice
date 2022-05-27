61003bbbf226d0e667b08002844e7796a0ffce78
Gooner12 <kcbiaksh@gmail.com>
2022-05-27 13:47:13 +1000

    Get product and get all product api is set up completely

--- 07-file-upload/starter/controllers/uploadsController.js
+++ 07-file-upload/starter/controllers/uploadsController.js
@@ -1,7 +1,9 @@
 const path = require('path');
 const {StatusCodes} = require('http-status-codes');
+const CustomError = require('../errors');
 
 const uploadProductImage = async(req, res) => {
+
     let productImage = req.files.image;
     const imagePath = path.join(__dirname,'../public/uploads/'+`${productImage.name}`);
 
